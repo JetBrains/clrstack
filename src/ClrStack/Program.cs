e6948bd8cdbd2affb6163f940d89f0ec654e75fa
Mikhail Filippov <mikhail@filippov.me>
2018-05-28 17:57:06 +0300

    Initial functionality

---
+++ src/ClrStack/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Microsoft.Diagnostics.Runtime;
+
+namespace ClrStack
+{
+  internal static class Program
+  {
+    public static void Main(string[] args)
+    {
+      if (args.Length != 1 || !int.TryParse(args[0], NumberStyles.Integer, CultureInfo.InvariantCulture,out var pid))
+      {
+        Console.Error.WriteLine("Usage: ClrStack.exe [PID]");
+        return;
+      }
+      using (var target = DataTarget.AttachToProcess(pid, 1000, AttachFlag.NonInvasive))
+      {
+        var clrVersion = target.ClrVersions.FirstOrDefault();
+        if (clrVersion == null)
+        {
+          Console.WriteLine($"CLR not found in process: {pid}");
+          return;
+        }
+        var runtime = clrVersion.CreateRuntime();
+
+        foreach (var clrThread in runtime.Threads)
+        {
+          if (!clrThread.IsAlive)
+            continue;
+          Console.WriteLine($"Thread #{clrThread.ManagedThreadId}:");
+
+          foreach (var frame in clrThread.StackTrace)
+            Console.WriteLine($"\tat {frame}");
+        }
+      }
+    }
+  }
+}
---
+++ src/ClrStack/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClrStack")]
+[assembly: AssemblyDescription("Tool for capture thread dump from .NET process")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mikhail Filippov")]
+[assembly: AssemblyProduct("ClrStack")]
+[assembly: AssemblyCopyright("Copyright © Mikhail Filippov 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+
+[assembly: Guid("2ECDA63E-31B6-4562-B88D-FDC51DCA1959")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
