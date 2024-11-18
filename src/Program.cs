cb68fd275bdc97e6a6fccc7c93a2969338641394
Ilya.Usov <Ilya.Usov@jetbrains.com>
2024-11-18 16:28:37 +0100

    Support CreateSnapshotAndAttach (for windows only)
    Support ability to capture process dumps

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
@@ -8,6 +9,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Diagnostics.NETCore.Client;
 using Microsoft.Diagnostics.Runtime;
 
 namespace ClrStack
@@ -15,6 +17,10 @@ namespace ClrStack
     internal static class Program
     {
         private const string ThreadDumpDirEnvVar = "THREAD_DUMP_DIR";
+        private const string NoSuspend = "--no-suspend";
+        private const string Timeout = "--timeout";
+        private const string DumpProcessTo = "--dump-process-to";
+        private const string CreateSnapshotAndAttach = "--create-snapshot-and-attach";
 
         [DllImport("kernel32.dll", SetLastError = true)]
         private static extern bool IsWow64Process(IntPtr hProcess, out bool isWow64Process);
@@ -49,7 +55,7 @@ namespace ClrStack
 
         private static int PrintUsageError()
         {
-            Console.Error.WriteLine("Usage: ClrStack.exe PID [--no-suspend] [--timeout ms]");
+            Console.Error.WriteLine($"Usage: ClrStack.exe PID  [{NoSuspend}] {{ [{Timeout}=5000 ms] [{CreateSnapshotAndAttach} (windows only)] }} | {{ {DumpProcessTo}=\"full path\" }}");
             return 1;
         }
 
@@ -61,26 +67,7 @@ namespace ClrStack
                 return PrintUsageError();
             }
 
-            bool suspend = true;
-            int? timeoutMs = null;
-            foreach (var arg in args.Skip(1))
-            {
-                if (arg.Equals("--no-suspend", StringComparison.InvariantCultureIgnoreCase) && suspend)
-                {
-                    suspend = false;
-                }
-                else if (arg.StartsWith("--timeout=", StringComparison.InvariantCultureIgnoreCase) && timeoutMs == null)
-                {
-                    if (!int.TryParse(arg.Split(new[] {'='}, 2)[1], NumberStyles.None, CultureInfo.InvariantCulture, out var value))
-                    {
-                        return PrintUsageError();
-                    }
-
-                    timeoutMs = value;
-                }
-                else
-                    return PrintUsageError();
-            }
+            args = args.Skip(1).ToArray(); // skip pid
 
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && Environment.Is64BitOperatingSystem && Environment.Is64BitProcess)
             {
@@ -96,63 +83,235 @@ namespace ClrStack
                 return 1;
             }
 
-            DataTarget target = null;
-            var timeoutCanceler = new CancellationTokenSource();
-            if (timeoutMs != null)
+            var processDumpParameters = TryParseProcessDumpParameters(args, pid);
+            if (processDumpParameters != null)
             {
-                Task.Factory.StartNew(async () =>
-                {
-                    await Task.Delay(timeoutMs.Value, timeoutCanceler.Token);
-
-                    // ReSharper disable once AccessToModifiedClosure
-                    target?.Dispose();
-
-                    await Console.Error.WriteLineAsync("Cannot capture stack trace from process: Timeout expired.");
-                    Environment.Exit(2);
-                }, timeoutCanceler.Token);
+                return ToResult(DumpProcess(processDumpParameters));
             }
 
-            var isExceptionThrown = false;
-            var output = new StringBuilder();
+            var threadDumpParameters = TryParseThreadDumpParameters(args, pid, threadDumpDir);
+            if (threadDumpParameters == null)
+            {
+                return PrintUsageError();
+            }
+
+            return ToResult(DumpStackTraces(threadDumpParameters));
+        }
+
+        private static bool TryParseTimeout(string arg, out int timeoutMs)
+        {
+            if (TryParseValue(arg, Timeout, out var value))
+            {
+                return int.TryParse(value, NumberStyles.None, CultureInfo.InvariantCulture, out timeoutMs);
+            }
+
+            timeoutMs = -1;
+            return false;
+        }
+
+        private static bool TryParseValue(string arg, string pattern, out string value)
+        {
+            const char separator = '=';
+            if (arg.Length > pattern.Length + 1 && arg.StartsWith(pattern, StringComparison.InvariantCultureIgnoreCase) && arg[pattern.Length] == separator)
+            {
+                value = arg.Split(new[] { separator }, 2)[1];
+                return true;
+            }
+
+            value = null;
+            return false;
+        }
+
+        private static bool TryParseFlag(string arg, string pattern)
+        {
+            return arg.Equals(pattern, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        private static ProcessDumpParameters TryParseProcessDumpParameters(string[] args, int pid)
+        {
+            var parameters = new ProcessDumpParameters(pid);
+            foreach (var arg in args)
+            {
+                if (TryParseTimeout(arg, out var timeoutMs) && parameters.TimeoutMs == null)
+                {
+                    parameters.TimeoutMs = timeoutMs;
+                }
+                else if (TryParseValue(arg, DumpProcessTo, out var dumpPath) && parameters.DumpPath == null)
+                {
+                    parameters.DumpPath = dumpPath;
+                }
+                else return null;
+            }
+
+            return parameters.Success ? parameters : null;
+        }
+
+        private static ThreadDumpParameters TryParseThreadDumpParameters(string[] args, int pid, string threadDumpDir)
+        {
+            var parameters = new ThreadDumpParameters(pid, threadDumpDir);
+            foreach (var arg in args)
+            {
+                if (TryParseFlag(arg, NoSuspend) && parameters.Suspend)
+                {
+                    parameters.Suspend = false;
+                }
+                else if (TryParseTimeout(arg, out var timeoutMs) && parameters.TimeoutMs == null)
+                {
+                    parameters.TimeoutMs = timeoutMs;
+                }
+                else if (TryParseFlag(arg, CreateSnapshotAndAttach) && !parameters.CreateSnapshotAndAttach && RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    parameters.CreateSnapshotAndAttach = true;
+                }
+                else return null;
+            }
+
+            return parameters;
+        }
+
+        private static int ToResult(bool success) => success ? 0 : 3;
+
+        private static bool DumpProcess(ProcessDumpParameters parameters)
+        {
             try
             {
-                using (target = DataTarget.AttachToProcess(pid, suspend))
+                var directory = new FileInfo(parameters.DumpPath).Directory;
+                if (directory == null) throw new DirectoryNotFoundException($"Parent of {parameters.DumpPath} is null");
+
+                if (!directory.Exists)
+                    directory.Create();
+
+                var path = parameters.DumpPath;
+                using (WithTimeoutCookie(parameters.TimeoutMs, () => { }))
                 {
-                    var clrVersion = target.ClrVersions.FirstOrDefault() ?? throw new Exception("CLR not found in process");
-
-                    using (var runtime = clrVersion.CreateRuntime())
-                    {
-                        foreach (var clrThread in runtime.Threads)
-                        {
-                            if (!clrThread.IsAlive)
-                                continue;
-                            output.AppendLine($"Thread #{clrThread.ManagedThreadId}:");
-
-                            foreach (var frame in clrThread.EnumerateStackTrace())
-                                output.AppendLine($"\tat {frame}");
-                        }
-                    }
+                    new DiagnosticsClient(parameters.Pid).WriteDump(DumpType.Full, path);
+                    return true;
                 }
-
-                timeoutCanceler.Cancel();
             }
             catch (Exception ex)
             {
-                isExceptionThrown = true;
-                output.AppendLine($"Cannot capture stack trace from process[{pid}]. Error: {ex.Message}");
+                Console.Error.WriteLine($"Cannot dump from process[{parameters.Pid}]. Error: {ex.Message}");
+                return false;
+            }
+        }
+
+        private static bool DumpStackTraces(ThreadDumpParameters parameters)
+        {
+            DataTarget target = null;
+            var success = true;
+            var output = new StringBuilder();
+
+            using (WithTimeoutCookie(parameters.TimeoutMs, () => target?.Dispose()))
+            {
+                try
+                {
+                    using (target = AttachToProcess(parameters))
+                    {
+                        var clrVersion = target.ClrVersions.FirstOrDefault() ?? throw new Exception("CLR not found in process");
+
+                        using (var runtime = clrVersion.CreateRuntime())
+                        {
+                            foreach (var clrThread in runtime.Threads)
+                            {
+                                if (!clrThread.IsAlive)
+                                    continue;
+                                output.AppendLine($"Thread #{clrThread.ManagedThreadId}:");
+
+                                foreach (var frame in clrThread.EnumerateStackTrace())
+                                    output.AppendLine($"\tat {frame}");
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    success = false;
+                    output.AppendLine($"Cannot capture stack trace from process[{parameters.Pid}]. Error: {ex.Message}");
+                }
             }
 
-            if (string.IsNullOrEmpty(threadDumpDir))
+            if (string.IsNullOrEmpty(parameters.ThreadDumpDir))
             {
                 Console.Write(output.ToString());
             }
             else
             {
                 var fileName = $"{DateTime.Now:yyyy-MM-dd_HH-mm-ss.fff}.tdump";
-                File.WriteAllText(Path.Combine(threadDumpDir, fileName), output.ToString(), Encoding.UTF8);
+                var path = Path.Combine(parameters.ThreadDumpDir, fileName);
+                File.WriteAllText(path, output.ToString(), Encoding.UTF8);
             }
 
-            return isExceptionThrown ? 3 : 0;
+            return success;
+        }
+
+        private static TimeoutCookie WithTimeoutCookie(int? timeoutMs, Action action = null)
+        {
+            if (timeoutMs == null) return default;
+
+            var source = new CancellationTokenSource();
+            var token = source.Token;
+            Task.Factory.StartNew(async () =>
+            {
+                await Task.Delay(timeoutMs.Value, token);
+
+                action?.Invoke();
+
+                await Console.Error.WriteLineAsync("Cannot capture stack trace from process: Timeout expired.");
+                Environment.Exit(2);
+            }, token);
+
+            return new TimeoutCookie(source);
+        }
+
+        private static DataTarget AttachToProcess(ThreadDumpParameters parameters)
+        {
+            return parameters.CreateSnapshotAndAttach
+                ? DataTarget.CreateSnapshotAndAttach(parameters.Pid)
+                : DataTarget.AttachToProcess(parameters.Pid, parameters.Suspend);
+        }
+    }
+
+    public class ProcessDumpParameters
+    {
+        public int Pid { get;  }
+        public string DumpPath { get; set; }
+        public int? TimeoutMs { get; set; } = null;
+
+        public bool Success => DumpPath != null;
+
+        public ProcessDumpParameters(int pid)
+        {
+            Pid = pid;
+        }
+    }
+
+    public class ThreadDumpParameters
+    {
+        public ThreadDumpParameters(int pid, string threadDumpDir)
+        {
+            Pid = pid;
+            ThreadDumpDir = threadDumpDir;
+        }
+
+        public int? TimeoutMs { get; set; } = null;
+        public int Pid { get;  }
+        public string ThreadDumpDir { get; }
+        public bool Suspend { get; set; } = true;
+        public bool CreateSnapshotAndAttach { get; set; } = false;
+    }
+
+    public readonly struct TimeoutCookie : IDisposable
+    {
+        private readonly CancellationTokenSource _source;
+
+        public TimeoutCookie(CancellationTokenSource source)
+        {
+            _source = source;
+        }
+
+        public void Dispose()
+        {
+            _source?.Dispose();
         }
     }
 }
